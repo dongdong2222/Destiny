d817df018f8530067e0a42251a3c557a1aa19427
Dongju Jang <71020187+dongdong2222@users.noreply.github.com>
2025-02-01 00:30:19 +0900

    Update Custom Primary GameModule

--- Source/Destiny/Destiny.cpp
+++ Source/Destiny/Destiny.cpp
@@ -3,4 +3,26 @@
 #include "Destiny.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Destiny, "Destiny" );
+class FDestinyGameModule : public FDefaultGameModuleImpl
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FDestinyGameModule::StartupModule()
+{
+	FDefaultGameModuleImpl::StartupModule();
+	UE_LOG(LogTemp, Log, TEXT("StartupModule"));
+}
+
+void FDestinyGameModule::ShutdownModule()
+{
+	FDefaultGameModuleImpl::ShutdownModule();
+	UE_LOG(LogTemp, Log, TEXT("ShutdownModule"));
+
+}
+
+//PRIMARY MODULE은 하나만 Implement 가능
+//추가적인 모듈은 IMPLEMENT_GAME_MODULE
+IMPLEMENT_PRIMARY_GAME_MODULE(FDestinyGameModule, Destiny, "Destiny" );
