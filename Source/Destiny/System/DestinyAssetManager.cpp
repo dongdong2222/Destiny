077fb69e74fd6d69675ac9fa8c3f9a3552ebceae
Dongju Jang <71020187+dongdong2222@users.noreply.github.com>
2025-02-02 16:30:51 +0900

    Add Custom Asset Manager

---
+++ Source/Destiny/System/DestinyAssetManager.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DestinyAssetManager.h"
+#include "Destiny/DestinyLogChannels.h"
+
+UDestinyAssetManager::UDestinyAssetManager()
+{
+}
+
+UDestinyAssetManager& UDestinyAssetManager::Get()
+{
+	check(GEngine);
+	//GEngine::TObjectPtr<class UAssetManager> AssetManager에 캐싱되어있다
+	if (UDestinyAssetManager* Singleton = Cast<UDestinyAssetManager>(GEngine->AssetManager))
+	{
+		return *Singleton;
+	}
+	UE_LOG(D_Log, Fatal, TEXT("invalid AssetManangerClass"));
+
+	return *NewObject<UDestinyAssetManager>();
+}
+
+bool UDestinyAssetManager::ShouldLogAssetLoads()
+{
+	const TCHAR* CommandLindContent = FCommandLine::Get();
+	static bool bLogAssetLoads = FParse::Param(CommandLindContent, TEXT("LogAssetLoads"));
+	return bLogAssetLoads;
+}
+
+
+UObject* UDestinyAssetManager::SynchronousLoadAsset(const FSoftObjectPath& AssetPath)
+{
+	//해당 함수를 따로 만든 이유는 'synchronous(동기식) load asset이 불필요하게 하는 것이 없는지 확인하기 위함
+	if (AssetPath.IsValid())
+	{
+		//FScopeLogTime을 확인 -> 로드시 얼마나 걸렸나
+		TUniquePtr<FScopeLogTime> LogTimePtr;
+		if (ShouldLogAssetLoads())
+		{
+			//로깅하면서, 초단위로 로깅
+			LogTimePtr = MakeUnique<FScopeLogTime>(*FString::Printf(TEXT("synchronous loaded assets [%s]"), *AssetPath.ToString()), nullptr, FScopeLogTime::ScopeLog_Seconds);
+		}
+		//동기적 로딩 : 로딩 시 게임 일시정지, 정적 로딩: 게임 시작 전 에셋이 로드된 상태로 존재
+
+		//2가지 분기
+		//1. assetManager가 있으면, AssetManager의 StreamableManager를 통해 정적 로딩
+		//2. 아니면, FSoftObjectPath를 통해 바로 정적 로딩
+		if (UAssetManager::IsValid())
+		{
+			return UAssetManager::GetStreamableManager().LoadSynchronous(AssetPath);
+		}
+
+		// if asset manager is not ready, use LoadObject()
+		// - 슥 보고 StaticLoadObject가 보인다 -> 캐시 되어있다.
+		// - 참고로, 항상 StaticLoaadObject를 하기 전에 StaticFindObject를 통해 확인하고 실패시 진짜 로딩함
+		return AssetPath.TryLoad();
+
+	}
+	return nullptr;
+}
+
+void UDestinyAssetManager::AddLoadedAsset(const UObject* Asset)
+{
+	if (ensureAlways(Asset))
+	{
+		FScopeLock LoadedAssetsLock(&LoadedAssetsCritical);
+		LoadedAssets.Add(Asset);
+	}
+}
---
+++ Source/Destiny/System/DestinyAssetManager.h
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "DestinyAssetManager.generated.h"
+
+/**
+ * PrimaryAsset?? : 
+ */
+UCLASS()
+class DESTINY_API UDestinyAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+public:
+	UDestinyAssetManager();
+
+	UDestinyAssetManager& Get();
+
+	static bool ShouldLogAssetLoads();
+
+	UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+	
+	template <class AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	template <class AssetType>
+	static TSubclassOf<AssetType> GetSubclass(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	void AddLoadedAsset(const UObject* Asset);
+
+	//GC의 대상
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	//Object 단위 locking
+	FCriticalSection LoadedAssetsCritical;
+};
+
+template<class AssetType>
+inline AssetType* UDestinyAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+
+	if (AssetPath.IsValid())
+	{
+		//Get() -> 로드되어 있다면 바로 가져옴
+		//      -> 로드되어 있지 않다면 Null
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedAsset && bKeepInMemory)
+		{
+			// Added to loaded asset list.
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+
+	return LoadedAsset;
+}
+
+template<class AssetType>
+inline TSubclassOf<AssetType> UDestinyAssetManager::GetSubclass(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<UClass>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset class [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			// Added to loaded asset list.
+			// AddLoadedAsst은 메모리에 상주하기 위한 장치
+			// 한 번 등록되면 직접 내리지 않는 한 Unload되지 않음( ** 캐싱), GC가 회수하지 않도록
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
+
